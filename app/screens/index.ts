c5fb5810586f014f8975b6033d73f9dfa71d1e7c
Simon Wacker <simon.wacker@gmail.com>
2021-06-22 12:19:07 +0200

    Add dummy screens generated with ignite generator

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -2,3 +2,8 @@ export * from "./welcome/welcome-screen"
 export * from "./demo/demo-screen"
 export * from "./demo/demo-list-screen"
 // export other screens here
+export * from "./classes/classes-screen"
+export * from "./poses/poses-screen"
+export * from "./music/music-screen"
+export * from "./playlists/playlists-screen"
+export * from "./settings/settings-screen"
