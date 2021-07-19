579242b1e445979c589bbaf591cf4a8d2d2b8d0d
Simon Wacker <simon.wacker@posteo.de>
2021-07-19 10:40:23 +0200

    Use `trackId` for clarity (it must be unique among all tracks, not only among classes)

--- app/screens/classes/classes-screen.tsx
+++ app/screens/classes/classes-screen.tsx
@@ -25,7 +25,7 @@ const FLAT_LIST: ViewStyle = {
 
 const classes = [
   {
-    id: "volume-1-part-1",
+    trackId: "volume-1-part-1",
     name: "Volume 1 - Teil 1 - Grundlegende Einführung",
     fileExtension: "mp3",
     md5FileHashValue: "4154d609e7307a3cc31c9ac1e20ea9d0",
@@ -33,7 +33,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.1&files=Yoga%20Insights%20Volume%201-Teil%201-Grundlegende%20Einf%C3%BChrung.mp3",
   },
   {
-    id: "volume-1-part-2",
+    trackId: "volume-1-part-2",
     name: "Volume 1 - Teil 2 - Regeneratives entlastendes Abendprogramm",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -41,7 +41,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.1&files=Yoga%20Insights%20Volume%201-Teil%202-Regeneratives%20entlastendes%20Abendprogramm.mp3",
   },
   {
-    id: "volume-1-part-3",
+    trackId: "volume-1-part-3",
     name: "Volume 1 - Teil 3 - Naturklänge zum freien Üben",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -49,7 +49,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.1&files=Yoga%20Insights%20Volume%201-Teil%203-Naturkl%C3%A4nge%20zum%20freien%20%C3%9Cben.mp3",
   },
   {
-    id: "volume-1-part-4",
+    trackId: "volume-1-part-4",
     name: "Volume 1 - Teil 4 - Langes Yogaprogramm Rückenstärkung und Rückenentlastung",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -57,7 +57,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.1&files=Yoga%20Insights%20Volume%201-Teil%204-Langes%20Yogaprogramm%20R%C3%BCckenst%C3%A4rkung%20und%20R%C3%BCckenentlastung.mp3",
   },
   {
-    id: "volume-2-part-1",
+    trackId: "volume-2-part-1",
     name: "Volume 2 - Teil 1 - Einführung",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -65,7 +65,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.2&files=Yoga%20Insights%20Volume%202-Teil%201-Einf%C3%BChrung.mp3",
   },
   {
-    id: "volume-2-part-2",
+    trackId: "volume-2-part-2",
     name: "Volume 2 - Teil 2 - Soforthilfe 1 & 2",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -73,7 +73,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.2&files=Yoga%20Insights%20Volume%202-Teil%202-Soforthilfe%201%262.mp3",
   },
   {
-    id: "volume-2-part-3",
+    trackId: "volume-2-part-3",
     name: "Volume 2 - Teil 3 - Musik pur",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -81,7 +81,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.2&files=Yoga%20Insights%20Volume%202-Teil%203-Musik%20pur.mp3",
   },
   {
-    id: "volume-2-part-4",
+    trackId: "volume-2-part-4",
     name: "Volume 2 - Teil 4 - Anleitung ohne Musik",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -89,7 +89,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.2&files=Yoga%20Insights%20Volume%202-Teil%204-Anleitung%20ohne%20Musik.mp3",
   },
   {
-    id: "volume-2-part-5",
+    trackId: "volume-2-part-5",
     name: "Volume 2 - Teil 5 - Ausführliches Übungsprogramm",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -97,7 +97,7 @@ const classes = [
       "https://citysoundstudio.de/n/index.php/s/WggwKH5eGSxzZbk/download?path=%2FYoga%20Insights%20Vol.2&files=Yoga%20Insights%20Volume%202-Teil%205-Ausf%C3%BChrliches%20%C3%9Cbungsprogramm.mp3",
   },
   {
-    id: "volume-2-part-6",
+    trackId: "volume-2-part-6",
     name: "Volume 2 - Teil 6 - Naturklänge",
     fileExtension: "mp3",
     md5FileHashValue: "?",
@@ -118,13 +118,13 @@ export const ClassesScreen = observer(function ClassesScreen() {
       <FlatList
         contentContainerStyle={FLAT_LIST}
         data={[...classes]}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.trackId}
         renderItem={({ item }) => (
           <Pressable
             onPress={() =>
               navigation.navigate("player", [
                 {
-                  trackId: item.id,
+                  trackId: item.trackId,
                   name: item.name,
                   fileExtension: item.fileExtension,
                   md5FileHashValue: item.md5FileHashValue,
