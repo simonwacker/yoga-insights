5f87ba0f3ad65004063dfbbdf2fae0195036776d
Simon Wacker <simon.wacker@posteo.de>
2021-08-07 11:37:57 +0200

    Remove superfluous content-container style

--- app/screens/select-tracks/select-tracks-screen.tsx
+++ app/screens/select-tracks/select-tracks-screen.tsx
@@ -1,16 +1,13 @@
 import React, { useCallback, useState } from "react"
 import { ViewStyle } from "react-native"
 import { Screen, SectionList, ListCheckboxItem, Button } from "../../components"
-import { color, spacing } from "../../theme"
+import { color } from "../../theme"
 import { useTrackStore } from "../../stores"
 import { SelectTracksScreenNavigationProp, SelectTracksScreenRouteProp } from "../../navigators"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.background,
   flex: 1,
-}
-const SECTION_LIST: ViewStyle = {
-  paddingHorizontal: spacing.medium,
 }
 
 export type SelectTracksScreenProps = {
@@ -37,7 +34,6 @@ export function SelectTracksScreen({ navigation }: SelectTracksScreenProps) {
     <Screen style={ROOT} preset="fixed">
       <SectionList
         getSectionTitle={(section) => section.title}
-        contentContainerStyle={SECTION_LIST}
         sections={poseSections}
         keyExtractor={(item) => item}
         renderItem={({ item }) => (
