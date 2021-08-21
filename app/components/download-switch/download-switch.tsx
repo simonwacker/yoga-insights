28f50431ed16bc8a59c4da1406389a25d2d1b87b
Simon Wacker <simon.wacker@posteo.de>
2021-08-21 09:54:25 +0200

    Remove unused import `useRef`

--- app/components/download-switch/download-switch.tsx
+++ app/components/download-switch/download-switch.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { DownloadResumable, FileSystemDownloadResult } from "expo-file-system"
 import { DownloadSwitchProps } from "./download-switch.props"
 import { FileSystem } from "react-native-unimodules"
