95766d37cf6ad1d15296add0725cbce0890eb7cd
Tim Habermaas <tim.habermaas@gmail.com>
2021-08-25 13:39:33 +0200

    [Code Review] Use RootParamList as generic parameter for NavigationContainerRef

--- app/navigators/navigation-utilities.tsx
+++ app/navigators/navigation-utilities.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef, useCallback } from "react"
 import { BackHandler } from "react-native"
 import { PartialState, NavigationState, NavigationContainerRef } from "@react-navigation/native"
 import * as storage from "../utils/storage"
+import { RootParamList } from "./root-navigator"
 
 export const RootNavigation = {
   navigate(name: string) {
@@ -14,9 +15,7 @@ export const RootNavigation = {
   },
 }
 
-export const setRootNavigation = (
-  ref: React.RefObject<NavigationContainerRef<Record<string, never>>>,
-) => {
+export const setRootNavigation = (ref: React.RefObject<NavigationContainerRef<RootParamList>>) => {
   for (const method in RootNavigation) {
     RootNavigation[method] = (...args: any) => {
       if (ref.current) {
@@ -48,7 +47,7 @@ export function getActiveRouteName(state: NavigationState | PartialState<Navigat
  * the navigation or allows exiting the app.
  */
 export function useBackButtonHandler(
-  ref: React.RefObject<NavigationContainerRef<Record<string, never>>>,
+  ref: React.RefObject<NavigationContainerRef<RootParamList>>,
   canExit: (routeName: string) => boolean,
 ) {
   const canExitRef = useRef(canExit)
